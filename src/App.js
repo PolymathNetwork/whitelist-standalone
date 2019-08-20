721a3364ffc6e09525f0be018b1a28775630f327
Remon Nashid <remon.sherin@gmail.com>
2019-08-20 15:47:15 -0400

    Put correct polyRegistry address on mainnet

--- src/App.js
+++ src/App.js
@@ -52,7 +52,7 @@ async function connect(dispatch) {
 
   const networkConfigs = {
     1: {
-      polymathRegistryAddress: '0x240f9f86b1465bf1b8eb29bc88cbf65573dfdd97'
+      polymathRegistryAddress: '0xdfabf3e4793cd30affb47ab6fa4cf4eef26bbc27'
     },
     42: {
       polymathRegistryAddress: '0x5b215a7d39ee305ad28da29bf2f0425c6c2a00b3'
