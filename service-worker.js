43c2da88532b3e418b1a4780cf6d68aaba112cb3
Remon Nashid <remon.sherin@gmail.com>
2019-08-23 09:17:24 -0400

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/white-label/precache-manifest.b9df9dd2054d6451ca7335e69f8f5b29.js"
+  "/white-label/precache-manifest.d60f894ff27de09910161d2cc79ba1f3.js"
 );
 
 self.addEventListener('message', (event) => {
