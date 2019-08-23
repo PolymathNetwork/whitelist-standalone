0c42b54a107ebfce1a3cbf09cac366319628bab5
Remon Nashid <remon.sherin@gmail.com>
2019-08-23 04:09:34 -0400

    An attempt with rc-form-hooks

---
+++ src/ShareholdersTable.js
@@ -0,0 +1,73 @@
+
+import React, { Fragment } from 'react';
+import { Table, Button, Typography, Icon } from 'antd';
+import { utils as web3Utils } from 'web3';
+import moment from 'moment';
+
+const {Column} = Table;
+const {Text} = Typography;
+
+function formatDate(input) {
+  return moment(input).format('YYYY-MM-DD');
+}
+  
+function formatBool(input) {
+  return input ? 
+    <Fragment><Icon style={{color: '#00AA5E'}} type="check-circle" theme="filled"/><span>   Yes</span></Fragment> :
+    <Fragment><Icon style={{color: '#DB2C3E'}} type="close-circle" theme="filled"/><span>   No</span></Fragment>;
+}
+
+export default ({shareholders, openForm}) => {
+  return (
+    <Table dataSource={shareholders} rowKey="address">
+      <Column
+        title='Address'
+        dataIndex='address'
+        key='address'
+        render={(text) => <Text>{web3Utils.toChecksumAddress(text)}</Text>}
+      />
+      <Column
+        title='Can send after'
+        dataIndex='canSendAfter'
+        key='canSendAfter'
+        render={(text) => formatDate(text)}
+      />
+      <Column
+        title='Can receive after'
+        dataIndex='canReceiveAfter'
+        key='canReceiveAfter'
+        render={(text) => formatDate(text)}
+      />
+      <Column
+        title='KYC expiry'
+        dataIndex='kycExpiry'
+        key='kycExpiry'
+        render={(text) => formatDate(text)}
+      />
+      <Column
+        title='Can buy from STO'
+        dataIndex='canBuyFromSto'
+        key='canBuyFromSto'
+        render={(text) => formatBool(text)}
+      />
+      <Column
+        title='Is accredited'
+        dataIndex='isAccredited'
+        key='isAccredited'
+        render={(text) => formatBool(text)}
+      />
+      <Column render={(text, record) => {
+        return (
+          <Fragment>
+            <Button onClick={() => openForm(record.address)}>
+              <Icon type="edit" theme="filled" />
+            </Button>
+            {/* <Button onClick={() => deleteShareholders([record])}>
+                  <Icon type="delete" theme="filled" />
+              </Button> */}
+          </Fragment>
+        )
+      }}/>
+    </Table>
+  )
+};
--- src/Whitelist.js
+++ src/Whitelist.js
@@ -1,11 +1,16 @@
-import React, { Fragment } from 'react';
+import React, { useEffect, useReducer } from 'react';
+import useForm from 'rc-form-hooks';
 import moment from 'moment';
 import { utils as web3Utils } from 'web3';
 import { Table, Button, Form, Input, DatePicker, Checkbox, Modal, Typography, Spin, Icon, message } from 'antd';
 
+import ShareholdersTable from './ShareholdersTable';
+
 const {Column} = Table;
 const {Item} = Form;
 const {Text} = Typography;
+
+
 
 const formItemLayout = {
   labelCol: {
@@ -18,15 +23,6 @@ const formItemLayout = {
   },
 };
 
-function formatDate(input) {
-  return moment(input).format('YYYY-MM-DD');
-}
-
-function formatBool(input) {
-  return input ? 
-    <Fragment><Icon style={{color: '#00AA5E'}} type="check-circle" theme="filled"/><span>   Yes</span></Fragment> :
-    <Fragment><Icon style={{color: '#DB2C3E'}} type="close-circle" theme="filled"/><span>   No</span></Fragment>;
-}
 
 const defaultShareholderValues = {
   address: '',
@@ -37,141 +33,82 @@ const defaultShareholderValues = {
   isAccredited: false
 }
 
-const WhitelistForm = Form.create({ name: 'form_in_modal' })(
-  class extends React.Component {
+const initialState = {
+  editIndex: '',
+  visible: false,
+  ongoingTx: false,
+}
 
-    render() {
-      const { visible,
-        onCancel,
-        onOk, form,
-        awaitingConfirmation,
-        editedRecord,
-        shareholderExists
-      } = this.props;
-      let defaultValues = editedRecord || defaultShareholderValues;
-            
-      const { getFieldDecorator } = form;
-      return ( 
-        <Modal
-          title={editedRecord ? "Edit shareholder" : "Add a new shareholder"}
-          okText="Save"
-          visible={visible}
-          onCancel={onCancel}
-          onOk={onOk}
-          confirmLoading={awaitingConfirmation}
-        >
-          <Spin spinning={awaitingConfirmation} size="large">
-            <Form {...formItemLayout}>
-              <Item name="address" label="Address">
-                {getFieldDecorator('address', {
-                  rules: [
-                    { required: true  },
-                    {
-                      validator: (rule, value, callback) => {
-                        if (!editedRecord && !web3Utils.isAddress(value)) {
-                          callback('Address is invalid')
-                          return;
-                        }
-                        callback()
-                        return;
-                      }
-                    },
-                    {
-                      validator: (rule, value, callback) => {
-                        if (!editedRecord && shareholderExists(value)) {
-                          callback('Shareholder is already present in the whitelist')
-                          return;
-                        }
-                        callback()
-                        return;
-                      }
-                    }
-                  ],
-                  initialValue: defaultValues.address
-                })(
-                  <Input disabled={!!editedRecord}/>
-                )}
-              </Item>
-              <Item name="canSendAfter"  label="Can Send after">
-                {getFieldDecorator('canSendAfter', {
-                  rules: [{ required: true }],
-                  initialValue: defaultValues.canSendAfter
-                })(
-                  <DatePicker />
-                )}
-              </Item>
-              <Item name="canReceiveAfter" label="Can Receive adter">
-                {getFieldDecorator('canReceiveAfter', {
-                  rules: [{ required: true }],
-                  initialValue: defaultValues.canReceiveAfter
-                })(
-                  <DatePicker />
-                )}
-              </Item>
-              <Item name="kycExpiry" label="KYC Expiry">
-                {getFieldDecorator('kycExpiry', {
-                  rules: [{ required: true }],
-                  initialValue: defaultValues.kycExpiry
-                })(
-                  <DatePicker />
-                )}
-              </Item>
-              <Item name="canBuyFromSto" label="Can Buy from STO">
-                {getFieldDecorator('canBuyFromSto', {
-                  valuePropName: 'checked',
-                  initialValue: defaultValues.canBuyFromSto
-                })(
-                  <Checkbox/>
-                )}
-              </Item>
-              <Item name="isAccredited" label="Accredited">
-                {getFieldDecorator('isAccredited', {
-                  valuePropName: 'checked',
-                  initialValue: defaultValues.isAccredited
-                })(
-                  <Checkbox />
-                )}
-              </Item>
-            </Form>
-          </Spin>
-        </Modal>
-      )}
-  }
-);
-
-export default class Whitelist extends React.Component {
-    state = {
+const reducer = (state, action) => {
+  switch (action.type) {
+  case 'OPEN_FORM':
+    const { editIndex } = action.payload;
+    return {
+      ...state,
+      visible: true,
+      editIndex
+    }
+  case 'CLOSE_FORM':
+    return {
+      ...state, 
       editIndex: '',
       visible: false,
-      awaitingConfirmation: false,
+      ongoingTx: false
     }
-
-    closeForm = () => {
-      this.setState({
-        editIndex: '',
-        visible: false,
-        awaitingConfirmation: false
-      });
-      const { form } = this.formRef.props;
-      form.resetFields();
+  case 'TX_SEND':
+    return {
+      ...state,
+      ongoingTx: true
     }
-
-    openForm = (index = '') => {
-      this.setState({
-        visible: true,
-        editIndex: index
-      });
+  case 'TX_RECEIPT':
+    return {
+      ...state,
+      ongoingTx: false,
+      visible: false,
+      error: '',
+      editIndex: ''
     }
+  case 'TX_ERROR':
+    const { error } = action.payload
+    return {
+      ...state,
+      error,
+      ongoingTx: false
+    }
+  default:
+    return state;
+  }
+}
 
-    submitForm = async () => {
-      const { form } = this.formRef.props;
-      const { modifyWhitelist } = this.props;
-      form.validateFields(async (err, values) => {
-        if (err) {
-          return;
-        }
+export default ({ 
+  modifyWhitelist, shareholders
+}) => {
+  const form = useForm();
+  const { getFieldDecorator, setFieldsValue, resetFields, values, errors, validateFields, setFields } = form;
+  console.log('values', values);
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const { visible, editIndex, ongoingTx } = state;
 
-        this.setState({ awaitingConfirmation: true })
+  const shareholderExists = (address) => {
+    const ret =  shareholders.find((element) => element.address.toUpperCase() === address.toUpperCase())
+         !== undefined
+    return ret
+  }
+
+  const closeForm = () => {
+    dispatch({type: 'CLOSE_FORM'});
+    resetFields();
+  }
+
+  const openForm = (index = '') => {
+    dispatch({ type: 'OPEN_FORM', payload: { editIndex: index } });
+  }
+
+  const submitForm = async () => {
+    validateFields(['address', 'canSendAfter', 'canReceiveAfter', 'kycExpiry', 'canBuyFromSto', 'isAccredited'], { force: true })
+      .then(async (values) => {
+        dispatch({type: 'TX_SEND'})
+        console.log('Submitted', values);
         
         values.canSendAfter = values.canSendAfter.toDate();
         values.canReceiveAfter = values.canReceiveAfter.toDate();
@@ -179,100 +116,116 @@ export default class Whitelist extends React.Component {
 
         try {
           await modifyWhitelist([values]);
-          this.setState({
-            visible: false,
-            awaitingConfirmation: false,
-            editIndex: ''
-          });
-          form.resetFields();
+          dispatch({ type: 'TX_RECEIPT'});
+          resetFields();
         }
         catch (error) {
-          this.setState({
-            awaitingConfirmation: false,
-          });
-          message.error(error.message)
+          console.error('Tx error', error)
+          dispatch({ type: 'TX_ERROR', payload: {error: error.message} });
+          message.error(error.message);
         }
       });
-    };
+  };
 
-    saveFormRef = formRef => {
-      this.formRef = formRef;
-    };
+  let editedRecord = shareholders.filter(shareholder => shareholder.address === editIndex)[0]
+  let defaultValues = editedRecord || defaultShareholderValues;
+  useEffect(() => {
+    // const values = getFieldsValue();
+    // console.log(values);
 
-    shareholderExists = (address) => {
-      const { shareholders } = this.props
-      const ret =  shareholders.find((element) => element.address.toUpperCase() === address.toUpperCase())
-         !== undefined
-      return ret
-    }
-    
-    render() {
-      const { visible, awaitingConfirmation, editIndex } = this.state;
-        
-      const { shareholders, deleteShareholders } = this.props;
-      let editedRecord = shareholders.filter(shareholder => shareholder.address === editIndex)[0]
-
-      return <div style={{display: 'flex', flexDirection: 'column'}}>
-        <Button type="primary" style={{marginBottom: 20, alignSelf: 'flex-end'}} onClick={() => this.openForm()}>Add new</Button>
-        <Table dataSource={shareholders} rowKey="address">
-          <Column
-            title='Address'
-            dataIndex='address'
-            key='address'
-            render={(text) => <Text>{web3Utils.toChecksumAddress(text)}</Text>}
-          />
-          <Column
-            title='Can send after'
-            dataIndex='canSendAfter'
-            key='canSendAfter'
-            render={(text) => formatDate(text)}
-          />
-          <Column
-            title='Can receive after'
-            dataIndex='canReceiveAfter'
-            key='canReceiveAfter'
-            render={(text) => formatDate(text)}
-          />
-          <Column
-            title='KYC expiry'
-            dataIndex='kycExpiry'
-            key='kycExpiry'
-            render={(text) => formatDate(text)}
-          />
-          <Column
-            title='Can buy from STO'
-            dataIndex='canBuyFromSto'
-            key='canBuyFromSto'
-            render={(text) => formatBool(text)}
-          />
-          <Column
-            title='Is accredited'
-            dataIndex='isAccredited'
-            key='isAccredited'
-            render={(text) => formatBool(text)}
-          />
-          <Column render={(text, record) => {
-            return (
-              <Fragment>
-                <Button onClick={() => this.openForm(record.address)}>
-                  <Icon type="edit" theme="filled" />
-                </Button>
-                {/* <Button onClick={() => deleteShareholders([record])}>
-                                <Icon type="delete" theme="filled" />
-                            </Button> */}
-              </Fragment>
-            )
-          }}/>
-        </Table>
-        <WhitelistForm 
-          wrappedComponentRef={this.saveFormRef}
-          visible={visible}
-          onCancel={this.closeForm}
-          onOk={this.submitForm}
-          awaitingConfirmation={awaitingConfirmation}
-          editedRecord={editedRecord}
-          shareholderExists={this.shareholderExists}
-        />
-      </div>;
-    }
+    setFieldsValue(defaultValues);
+  }, [defaultValues, setFieldsValue]);
+  
+  return (
+    <div style={{display: 'flex', flexDirection: 'column'}}>
+      <Button type="primary" style={{marginBottom: 20, alignSelf: 'flex-end'}} onClick={openForm}>Add new</Button>
+      <ShareholdersTable shareholders={shareholders} openForm={openForm} />
+      <Modal
+        title={editedRecord ? "Edit shareholder" : "Add a new shareholder"}
+        okText="Save"
+        closable={false}
+        visible={visible}
+        footer={null}
+      >
+        <Spin spinning={ongoingTx} size="large">
+          <Form {...formItemLayout}>
+            <Item name="address" label="Address">
+              {getFieldDecorator('address', {
+                rules: [
+                  { required: true  },
+                  {
+                    validator: (rule, value, callback) => {
+                      if (!editedRecord && !web3Utils.isAddress(value)) {
+                        callback('Address is invalid')
+                        return;
+                      }
+                      callback()
+                      return;
+                    }
+                  },
+                  {
+                    validator: (rule, value, callback) => {
+                      if (!editedRecord && shareholderExists(value)) {
+                        callback('Shareholder is already present in the whitelist')
+                        return;
+                      }
+                      callback()
+                      return;
+                    }
+                  }
+                ],
+                // initialValue: defaultValues.address
+              })(
+                <Input disabled={!!editedRecord}/>
+              )}
+            </Item>
+            <Item name="canSendAfter"  label="Can Send after">
+              {getFieldDecorator('canSendAfter', {
+                rules: [{ required: true }],
+                // initialValue: defaultValues.canSendAfter
+              })(
+                <DatePicker />
+              )}
+            </Item>
+            <Item name="canReceiveAfter" label="Can Receive adter">
+              {getFieldDecorator('canReceiveAfter', {
+                rules: [{ required: true }],
+                // initialValue: defaultValues.canReceiveAfter
+              })(
+                <DatePicker />
+              )}
+            </Item>
+            <Item name="kycExpiry" label="KYC Expiry">
+              {getFieldDecorator('kycExpiry', {
+                rules: [{ required: true }],
+                // initialValue: defaultValues.kycExpiry
+              })(
+                <DatePicker />
+              )}
+            </Item>
+            <Item name="canBuyFromSto" label="Can Buy from STO">
+              {getFieldDecorator('canBuyFromSto', {
+                valuePropName: 'checked',
+                // initialValue: defaultValues.canBuyFromSto
+              })(
+                <Checkbox />
+              )}
+            </Item>
+            <Item name="isAccredited" label="Accredited">
+              {getFieldDecorator('isAccredited', {
+                valuePropName: 'checked',
+                // initialValue: defaultValues.isAccredited
+              })(
+                <Checkbox />
+              )}
+            </Item>
+            <Item>
+              <Button onClick={closeForm}>cancel</Button>
+              <Button onClick={submitForm}>save</Button>
+            </Item>
+          </Form>
+        </Spin>
+      </Modal>
+    </div>
+  );
 }
